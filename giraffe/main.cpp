3da3a686924573deee9419a33396693b9e8b5bc0
Fabricio de Almeida Guerra de Melo <96056721+FabriicioMelo@users.noreply.github.com>
2023-10-12 10:31:42 -0300

    Variables

--- giraffe/main.cpp
+++ giraffe/main.cpp
@@ -5,9 +5,17 @@
 using namespace std;
 
 int main() {
-    cout << "   /|" << endl;
-	cout << "  / |" << endl;
-	cout << " /  |" << endl;
-	cout << "/___|" << endl;
-    return 0;
+	string character_name = "Tom";
+	int character_age;
+	character_age = 50;
+	
+	cout << "There was a man named " << character_name << endl;
+	cout << "He was " << character_age << " years old" << endl;
+	
+	character_name = "Mike";
+	
+	cout << "He liked the name " << character_name << endl;
+	cout << "But did not like being " << character_age << endl;
+	
+	return 0;
 }
