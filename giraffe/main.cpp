4414f29e510890ee29237b1b34b92fcc917a9082
Fabricio de Almeida Guerra de Melo <96056721+FabriicioMelo@users.noreply.github.com>
2023-10-12 15:14:06 -0300

    Working with Strings

--- giraffe/main.cpp
+++ giraffe/main.cpp
@@ -5,13 +5,10 @@
 using namespace std;
 
 int main() {
-	char grade = 'A';
 	string phrase = "Giraffe Academy";
-	int age = 50;
-	double gpa = 2.3;
-	bool isMale = true;
-	
-	//cout <<  << endl;
+	string phrasesub;
+	phrasesub = phrase.substr(8, 3);
+	cout << phrasesub;
 	
 	return 0;
 }
