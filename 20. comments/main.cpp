abc1ab9ec0a7bbbffab69984e7e32ca2e4b934d2
Fabricio de Almeida Guerra de Melo <96056721+FabriicioMelo@users.noreply.github.com>
2023-11-14 14:18:35 -0300

    Comments

---
+++ 20. comments/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//Comentários são legais
+	
+	/*
+	
+	Também existem esses tipos de comentários!
+
+	*/
+
+	cout << "Comments are fun"; //Imprime no console
+
+	return 0;
+}
